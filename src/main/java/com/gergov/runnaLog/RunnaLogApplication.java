661d6c633afba085e9fe15994cfcc7f31df25a95
marti <martingergov@gmail.com>
2025-09-23 16:14:58 +0300

    chore: initial spring boot project setup

---
+++ src/main/java/com/gergov/runnaLog/RunnaLogApplication.java
@@ -0,0 +1,13 @@
+package com.gergov.runnaLog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunnaLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunnaLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gergov/runnaLog/RunnaLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gergov.runnaLog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunnaLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
